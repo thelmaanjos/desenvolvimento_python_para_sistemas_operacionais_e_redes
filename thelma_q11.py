64ead2730e41dbc71b982342b6ec1f708a09bd27
unknown <53521611+thelmaanjos@users.noreply.github.com>
2021-10-25 17:23:46 -0300

    enhance question 11

--- thelma_q11.py
+++ thelma_q11.py
@@ -1,27 +1,20 @@
 import os
 
 def get_user_input():
-    file_name = input("Enter the file name:")
+    file_name = input("Enter the file name: ")
     return file_name
 
 def doesFileExist(file_name):
   if os.path.exists(file_name):
     print(f'{file_name} exists!')
-    print(os.path.abspath(file_name))
+    print(os.path.abspath(file_name))  
+    open_file = "notepad.exe " + file_name
+    os.system(open_file)
   else:
     print(f'{file_name} doesn\'t exist!')
-
-def isFile(file_name):
-  if os.path.isfile(file_name):
-      print(f'{file_name} is a file!')
-  else:
-      print(f'{file_name} is not a file!')
 
 def main():
     file_name = get_user_input()
     doesFileExist(file_name)
-    isFile(file_name)
-    
-  
 
 main()
